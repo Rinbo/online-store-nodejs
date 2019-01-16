885aeaf9fe0da75c6d321c7eb708814c5b25c351
Robin Borjesson <robin.b@outlook.com>
2019-01-16 21:48:08 +0100

    Scaffold the app and create an http server

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+const server = require("./lib/server");
+
+// Declare the app
+const app = {};
+
+// Init function
+app.init = function() {
+  // Start the server
+  server.init();
+};
+
+// Self executing
+app.init();
+
+// Export the app
+module.exports = app;
